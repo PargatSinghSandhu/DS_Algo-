51d0ff0ba18c2434dbb1cf5a472742f9b053b71a
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-05 23:42:52 -0700

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct Node 
 {
     int data;
-    struct Node*next;                                                               // Self-initialization strcuture 
+    struct Node*next;                                                                    // Self-initialization strcuture 
 }*first=NULL;                                                                            //Pointer for impmenting a Linked List
 
 void create(int arr[], int n)
