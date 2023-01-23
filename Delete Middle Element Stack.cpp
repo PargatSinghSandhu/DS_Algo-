d1cf70a8ff0d4404516add05561e6d87a5e6c7cc
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-23 15:03:56 -0700

    Update Delete Middle Element Stack.cpp

--- Delete Middle Element Stack.cpp
+++ Delete Middle Element Stack.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stack>
 
 using namespace std;
 
@@ -16,4 +17,22 @@ void del()
     s.push(temp)
     
 }
-
+int main()
+{
+    int arr[] = {1,2,3,4,5}, s[5], top = -1;
+    
+    for (int i=0;i<5;i++)
+    {
+        top++;
+        s[top]=arr[i];
+        
+    }
+    if(s.size()==0)
+    {
+        return s;
+    }
+    int k=((s.size()/2)+1);
+    del(s,k);
+    return s;
+    
+}
