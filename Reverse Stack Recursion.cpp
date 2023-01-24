cefd04f9a4146e5291d32eaaff5c5ea706ac16f0
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-24 14:20:34 -0700

    Update Reverse Stack Recursion.cpp

--- Reverse Stack Recursion.cpp
+++ Reverse Stack Recursion.cpp
@@ -1,44 +1,41 @@
+
+
 #include<iostream>
 #include<stack>
 
 using namespace std;
 
-void insert(stack<int> &s, int ele)
+
+void del(stack<int>&s, int k)
 {
+    if(k==1)
+    {
+        s.pop();
+        return;
+    }
+    int temp= s.top();
+    s.pop();
+    del(s,k-1);
+    s.push(temp);
     
+}
+
+stack <int> delmid(stack<int>&s)
+
+{
     if(s.size()==0)
     {
-        s.push(ele);
-        return;
+        return s;
     }
+    int k=((s.size()/2)+1);
+    del(s,k);
     
-    int temp = s.top();
-    s.pop();
-    insert(s,ele);
-    s.push(temp);
-    return;
-    
+    return s;
 }
 
-
-
-void reverse(stack<int>s)
+void print(stack<int>&s)
 {
-    if(s.size()==1)
-    {
-        return;
-    }
-    int temp=s.top();
-    s.pop();
-    reverse(s);
-    insert(s,temp);
-    return;
-
     
-}
-
-void print(stack<int>s)
-{
     if(s.empty())
     {
         return;
@@ -49,20 +46,24 @@ void print(stack<int>s)
     print(s);
     s.push(x);
 }
+
 int main()
 {
     
-    
-    int arr[]={1,2,3,4,5}, top=-1;
+    int arr[] = {1,2,3,4,5}, top = -1;
     stack<int>s;
-    for(int i=0;i<5;i++)
+    
+    for (int i=0;i<5;i++)
     {
         top++;
         s.push(arr[i]);
+        
     }
-
-    reverse(s);
+    delmid(s);
     print(s);
     return 0;
-
+    
 }
+
+
+
