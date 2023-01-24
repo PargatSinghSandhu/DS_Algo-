5bbf42484e3f976ea55d277e9cb87952bfc577ff
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-24 13:55:31 -0700

    Update Delete Middle Element Stack.cpp

--- Delete Middle Element Stack.cpp
+++ Delete Middle Element Stack.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 void del(stack<int>&s, int k)
 {
+    cout<<"1";
     if(k==1)
     {
         s.pop();
@@ -16,6 +17,7 @@ void del(stack<int>&s, int k)
     s.pop();
     del(s,k-1);
     s.push(temp);
+    
     
 }
 
@@ -28,14 +30,27 @@ stack <int> delmid(stack<int>s)
     }
     int k=((s.size()/2)+1);
     del(s,k);
+    cout<<"1";
     return s;
+}
+
+void print(stack<int>s)
+{
+    int x= s.top();
+    s.pop();
+    cout<<x;
+    print(s);
+    s.push(x);
+    
+    
 }
 
 int main()
 {
+    cout<<"1000";
     int arr[] = {1,2,3,4,5}, top = -1;
     stack<int>s;
-    
+    cout<<"1000";
     for (int i=0;i<5;i++)
     {
         top++;
@@ -43,6 +58,8 @@ int main()
         
     }
     delmid(s);
+    print(s);
+    
     return 0;
     
 }
