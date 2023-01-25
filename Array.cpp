ea909eb338ef8bc5f106ce74c789deee9698fb17
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-25 13:52:27 -0700

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+{
+    int arr[5]={1,2,3,4,5};         //array defined in stack 
+    int *p, *q;                     //pointers will be used to define and access the array in heap.
+    
+    return 0;
+}
