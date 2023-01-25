835d6f1a07d7dae2a62fe8192da687886b080241
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-25 14:00:29 -0700

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,11 +1,35 @@
 #include<iostream>
-#include<stack>
+
 
 using namespace std;
-
+int main()
 {
     int arr[5]={1,2,3,4,5};         //array defined in stack 
     int *p, *q;                     //pointers will be used to define and access the array in heap.
+    
+    p = new int [5];
+    q=new int [10];
+    for(int j=0;j<5;j++)
+    {
+        cin>>p[j];
+    }
+    for(int k=0;k<5;k++)
+    {
+        q[k]=p[k];
+    }
+   
+    
+    
+    
+    delete []p;
+    p=q;
+    q=NULL;
+    
+    for(int i=0;i<5;i++)
+    {
+        cout<<p[i];
+    }
+    
     
     return 0;
 }
