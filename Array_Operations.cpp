0b731c7055896fbf40ba18422381064071881a7b
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-01-30 14:15:32 -0700

    Update Array_Operations.cpp

--- Array_Operations.cpp
+++ Array_Operations.cpp
@@ -22,16 +22,33 @@ void Display(struct Array arr)
 
 void Append(struct Array *arr, int x)  //Using call by address/refernce because wants to append changes will reflect in origianl array too.
 {
+    
     if(arr->length<arr->size)
     {
         arr->A[arr->length++]=x;
     }
+    
+}
+
+void Insert(struct Array *arr, int index, int x)
+{
+    
+    if(index>=0 && index<=arr->length)
+    for(int j=arr->length;j>index;j--)
+    {
+        arr->A[j]=arr->A[j-1];
+    }
+    arr->A[index]=x;
+    arr->length++;
+    
+    
 }
 
 int main()
 {
     struct Array arr = {{2,4,5,6,7},20,5};
     Append(&arr,15);
+    Insert(&arr,3,90);
     Display(arr);
     
     return 0;
