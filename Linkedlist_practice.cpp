33f15bcbc2e064c7fa40d48eea17df29951e4d8f
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-02-08 00:21:48 -0700

    Update Linkedlist_practice.cpp

--- Linkedlist_practice.cpp
+++ Linkedlist_practice.cpp
@@ -1,54 +1,32 @@
-#include<iostream>
-
-using namespace std;
-struct Node
+struct Node 
 {
-    int data;
-    Node*next;
-};
+    int data; 
+    struct Node*next; 
+}
 
-int main()
+struct Node*head;
+void Insert()
 {
-    Node* A;
-    A = NULL;
-    
-    Node*temp = new Node();
-    //Node*temp1= new Node;
-    
-    temp->data = 2;
-    temp->next=NULL;
-    A=temp;
-    temp= new Node();
-    
-    temp->data =3;
-    temp->next=NULL;
-
-   
-    
-    Node*temp1=A;
-    
-    
-    while(temp1->next!=NULL)
+    Node*temp = new Node;
+    temp->data = x;
+    temp->next = NULL;
+    head = temp;
+    if(head!=NULL)
     {
         
-        temp1=temp1->next;
-        
-        
     }
+}
 
-    temp1->next=temp;
-    
-    
-    
-    while(temp1!=NULL)
-    {
-        
-        cout<<temp1->data;
-        temp1=temp1-> next;
-        
-        
-    }
-    //cout<<temp1;
-   
-    return 0;
+int main ()
+{
+   head=NULL;
+   int n,x;
+   cout<<"Enter the count of Linkedlist";
+   cin>>n;
+   cout<<"Enter the item";
+   for(int i=0;i<n;i++)
+   {
+       cin>>x;
+   }
+   Insert(x);
 }
