d180a057be79e3e1a5aea4a139dd50dc9fef0041
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-02-09 17:24:34 -0700

    Update and rename Inserting Node LinkedList.cpp to Inserting Node nth LinkedList.cpp

---
+++ Inserting Node nth LinkedList.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node*next;
+};
+
+struct Node*head;
+
+void Insert()
+{
+    
+}
+
+void Print()
+{
+    
+}
+int main()
+{
+    head=NULL;
+}
