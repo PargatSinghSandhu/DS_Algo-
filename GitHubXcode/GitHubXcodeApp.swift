3c06f3ae80025c91cf82985fe87783542f3ceb7c
Pargat Singh <sandhu.pargat07@gmail.com>
2023-02-14 11:30:03 -0700

    Initial Commit

---
+++ GitHubXcode/GitHubXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubXcodeApp.swift
+//  GitHubXcode
+//
+//  Created by Pargat Singh on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
