df9c43129115c92c70de82257c251adf78605d2c
Pargat Singh <sandhu.pargat07@gmail.com>
2023-02-14 12:27:34 -0700

    First Commit

---
+++ Reverse_LinkedList.cpp
@@ -0,0 +1,27 @@
+//
+//  Reverse_LinkedList.cpp
+//  DSA
+//
+//  Created by Pargat Singh on 2/14/23.
+//
+
+#include "Reverse_LinkedList.hpp"
+
+
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    int Node*next;
+};
+
+struct Node*head;
+
+int reverse()
+{
+    Struct Node *prev, *current, *next;
+    
+}
