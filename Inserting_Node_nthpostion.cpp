b4a456024c645c46e89336d75854ad006ea63275
Pargat Singh <sandhu.pargat07@gmail.com>
2023-02-15 12:53:06 -0700

    Inserting Node at nth position

--- Inserting_Node_nthpostion.cpp
+++ Inserting_Node_nthpostion.cpp
@@ -16,31 +16,36 @@ struct Node
     struct Node*next;
 };
 struct Node*head;
+
+
+
+
+
 void Insert(int data, int n)
 {
-    Node*temp1= new Node();
-    temp1->data=data ;
-    temp1->next=NULL;
+    struct Node*temp1= new Node;
+    temp1->data = data;
+    temp1->next= NULL;
     
     if(n==1)
     {
         temp1->next=head;
-        head = temp1;
+        head=temp1;
         return;
     }
-    
-    Node * temp2 = head;
-    for(int i = 0;i<n-2;i++)
+    struct Node*temp2 = head ;
+    for(int i = 0 ; i<n-2;i++)
     {
         temp2=temp2->next;
     }
+    
     temp1->next=temp2->next;
     temp2->next=temp1;
     
     
 }
 
-void Print (struct Node* p)
+void Print(struct Node*p)
 {
     if(p==NULL)
     {
@@ -49,6 +54,7 @@ void Print (struct Node* p)
     
     cout<<p->data;
     Print(p->next);
+    
     
 }
 
