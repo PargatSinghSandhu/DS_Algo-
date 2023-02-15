5863ad6266d8e915059e2954da9728656c51246c
Pargat Singh <sandhu.pargat07@gmail.com>
2023-02-15 09:58:47 -0700

    Inserting Node at beginning

---
+++ Inserting_Node_Begining.cpp
@@ -0,0 +1,19 @@
+//
+//  Inserting_Node_Begining.cpp
+//  DSA
+//
+//  Created by Pargat Singh on 2/15/23.
+//
+
+#include <stdio.h>
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node*next;
+};
+
+
