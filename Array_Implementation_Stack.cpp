bb5bd96a57717a2fc2ff6ee352b6f9f6bf4a5fd7
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-02-24 09:29:50 -0700

    Create Array_Implementation_Stack.cpp

---
+++ Array_Implementation_Stack.cpp
@@ -0,0 +1,49 @@
+
+
+#include<iostream>
+#define MAXSIZE 100
+using namespace std;
+
+int A[MAXSIZE];
+
+int top=-1;
+
+void push(int x)
+{
+    if(top==MAXSIZE-1)
+    {
+        cout<<"Stack is overflow";
+        return;
+    }
+    A[++top]=x;
+    
+}
+
+void pop()
+{
+   if(top==-1)
+   {
+       cout<<"Stack is empty";
+       return;
+   }
+   top--;
+}
+
+void print()
+{
+    cout<<"Stack: ";
+    for(int i=0;i<=top;i++)
+    {
+        cout<<A[i];
+    }
+}
+
+int main()
+{
+    push(2);
+    
+    push(3);
+    pop();
+    print();
+    
+}
