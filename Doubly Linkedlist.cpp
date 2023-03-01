60bca77cbbc30d66a3b2de9f64c15e4a0f04b8c3
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-03-01 10:22:08 -0700

    Update Doubly Linkedlist.cpp

--- Doubly Linkedlist.cpp
+++ Doubly Linkedlist.cpp
@@ -1,7 +1,54 @@
+//
+//  Doubly_Linkedlist.cpp
+//  DSA
+//
+//  Created by Pargat Singh on 3/1/23.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
 struct Node
 {
-  int data;
-  struct Node* next;
-  struct Node* prev;
-};  
-  
+    int data;
+    struct Node * next;
+    struct Node *prev;
+};
+
+struct Node*head;
+
+struct Node*GetNewNode(int x)
+{
+    struct Node* newNode = new Node;
+    
+    newNode->data=x;
+    newNode->next=NULL;
+    newNode->prev=NULL;
+}
+
+void Insert(int x)
+{
+    struct Node*temp=getNewNode(x);
+    
+    if(head==NULL)
+    {
+        head=temp;
+        return;
+    }
+    //head->next=NULL;
+    //temp->next=head;
+    //head=temp;
+    head->prev=temp;
+    temp->next=head;
+    head=temp;
+    
+    
+}
+
+int main()
+{
+    
+}
