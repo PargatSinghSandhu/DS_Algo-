442c5156d8e22f9ab2b93f3a955cb188181a9b51
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-03-03 11:24:46 -0700

    Create Linkedlist_Stack.cpp

---
+++ Linkedlist_Stack.cpp
@@ -0,0 +1,18 @@
+//
+//  Linkedlist_Stack.cpp
+//  DSA
+//
+//  Created by Pargat Singh on 3/3/23.
+//
+
+#include <stdio.h>
+#include<iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node*next;
+    
+}
