0d42cd9a12d90fa547aad7bdd7f5a313d188e5b9
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-04-04 19:05:52 -0600

    Update Second largest element without sorting.cpp

--- Second largest element without sorting.cpp
+++ Second largest element without sorting.cpp
@@ -9,7 +9,12 @@ for (int i=0; i<n;i++)
 {
     if(a[i]>largest)
     {
+        slargest = largest;
+        largest = a[i];
+    }
+    else if( a[i]<largest && a[i]>slargest)
+    {
         slargest = a[i];
-        largest = slargest;
     }
 }
+return slargest;
