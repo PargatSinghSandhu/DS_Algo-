6d54fceab03ba753f8c02a424a94d414036b2a74
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-04-06 13:59:49 -0600

    Update Binary Search Tree.cpp

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -1,4 +1,3 @@
-
 #incldue<isotream>
 
 struct BstNode
@@ -7,3 +6,50 @@ struct BstNode
     BstNode *left;
     BstNode *right;
 };
+
+
+BstNode * GetNewNode(int data)
+{
+    BstNde* newNode  = new BstNode();
+    newNode->data = data;
+    newNode->left=data;
+    newNode->right=NULL;
+    return newNode;
+}
+
+BstNode*Insert( BstNode*root, int data)
+{
+   if(root == NULL)
+   {
+       root = GetNewNode(data);
+       return root;
+   }
+    else if(data<=root->data)
+    {
+        root->left = Insert(root->left,data);
+    }
+   
+    else {
+        root->right =Insert(root->right,data);
+    }
+    return root;
+}
+
+bool Search( BstNode* root, int data )
+{
+    if(root==NULL)
+        return false;
+    else if (root->data == data )
+        return true;
+    else if(data<=root->data)
+        return Search(root->left, data);
+    else return Search (root->right,data);
+}
+
+int main()
+{
+    BstNode*root=NULL;
+    root = Insert(root, 15);
+    root = Insert(root, 25);
+    root = Insert(root, 30);
+}
