d39a77da32963d366a3175b673d2646b56e36b06
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-04-13 12:14:06 -0600

    Update Check_Binary_Tree.cpp

--- Check_Binary_Tree.cpp
+++ Check_Binary_Tree.cpp
@@ -1,4 +1,3 @@
-
 #include<iostream>
 
 using namespace std;
@@ -11,7 +10,28 @@ struct Node
 };
 
 
+bool IsBinarySearchTree(Node*root)
+{
+    if(root==NULL)
+        return true;
+    if((IsSubtreelesser(root->left, root->data))&&IsSubtreegreater(root->right, root->data))&&IsBinarySearchTree(root->left)&&IsBinarySearchTree(root->right))
+        return true;
+}
 
+
+bool IsSubtreelesser(Node*root, int value)
+{
+    if(root==NULL)
+    {
+        return true;
+    }
+    if((root->data<=value) && IsSubtreelesser(root->left, value) && IsSubtreelesser(root->right, value))
+    {
+        return true;
+    }
+    else:
+        return false;
+}
 
 int main()
 {
