f57f67414a07a1583228ee2dbe2ec150683bfe31
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-04-17 09:03:53 -0600

    Create Left rotate array by 1 place.cpp

---
+++ Left rotate array by 1 place.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+void left_rotate_one(int arr, int n)
+
+{
+    int temp = arr[0];
+    
+}
+
+int main()
+{
+    int arr[6]={1,2,10,23,4,12};
+    left_rotate_one(arr,6);
+    
+}
