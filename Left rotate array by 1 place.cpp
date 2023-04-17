9001cd6b8d12b6042c3f03f1b4b4c1cdcf9556f4
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-04-17 09:06:52 -0600

    Update Left rotate array by 1 place.cpp

--- Left rotate array by 1 place.cpp
+++ Left rotate array by 1 place.cpp
@@ -2,11 +2,19 @@
 
 using namespace std;
 
-void left_rotate_one(int arr, int n)
+void left_rotate_one(int arr[], int n)
 
 {
     int temp = arr[0];
-    
+    for(int i=1;i<n;i++)
+    {
+        arr[i-1]=arr[i];
+    }
+    arr[n-1]=temp;
+    for(int j=0;j<n;j++)
+    {
+        cout<<arr[j]<<" ";
+    }
 }
 
 int main()
