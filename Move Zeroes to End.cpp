23cc236b1639704e0b9e4122c51461a809725ac6
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-04-20 10:25:36 -0600

    Create Move Zeroes to End.cpp

---
+++ Move Zeroes to End.cpp
@@ -0,0 +1,25 @@
+#inlcude<isotream>
+
+using namespace std;
+
+
+void Move_zeroes_end(int arr[], int n)
+{
+    int temp[];
+    for (int i=0;i<n;i++)
+    {
+        if(arr[i]!=0)
+        {
+            temp.add[i];
+        }
+    }
+    
+    
+}
+
+
+int main ()
+{
+    int arr[] = {1,0,2,10,5,0,0,3};
+    Move_zeroes_end(arr[], 6)
+}
