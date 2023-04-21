62cfd9826d1d10b4e62e3e731e2c6b3162ac4dae
PargatSinghSandhu <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-04-21 14:27:17 -0600

    Create Union of two sorted array optimal.cpp

---
+++ Union of two sorted array optimal.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<array>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+union_two_array(int arr1[], int arr2[], int n1, int n2)
+{
+    vector <int> union_arr;
+    while(int i<n1 && int j<n2)
+    {
+        if(arr1[i]<=arr2[j])
+        {
+            if(union_arr.back()!=arr[i])
+            {
+                union_arr.push_back(arr[i]);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int arr1[]={1,1,2,3,4,5};
+    int arr2[]={1,2,3,4,5,6};
+    
+    int n1 = end(arr1)-begin(arr1);
+    int n2 = end(arr2)-begin(arr2);
+    
+    union_two_array(arr1, arr2, n1, n2);
+}
