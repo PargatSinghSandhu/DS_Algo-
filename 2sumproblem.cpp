fe4aa18589f53c192c73c48ecd4c209d0930f6ee
Pargat Singh <sandhu.pargat07@gmail.com>
2023-05-17 12:54:50 -0600

    Hash and 2 sum

--- 2sumproblem.cpp
+++ 2sumproblem.cpp
@@ -1,9 +1,40 @@
-#include<isotream>
+#include<iostream>
+
+#include<map>
+#include<vector> 
 
 using namespace std;
 
+void twoSum(vector<int>& nums, int target)
+{
+ map<int, int> mymap;
+ 
+ for( int i = 0;i<nums.size();i++)
+ {
+    int first = nums[i];
+    int second = target - first;
+    //cout<<second;
+    if(mymap.find(second)!=mymap.end())
+    {
+        cout<<mymap[second]<<i;
+    }
+    else 
+    mymap[first]=i;
+
+ }
+
+  
+}
 int main()
 {
-    int arr[5]={1,2,3,4,5};
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(6);
+    nums.push_back(5);
+    nums.push_back(8);
+    nums.push_back(11);
+    int target=14;
+    twoSum(nums, 14);
+    
     return 0 ;
-    }
+}
---
+++ hashmap.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+void hash1()
+{
+    map<int, int> mymap;
+    mymap[1]=10;
+    mymap[2]=20;
+    mymap[3]=30;
+
+    for(auto it=mymap.begin(); it!=mymap.end(); ++it)
+    {
+        cout<<it->first;
+    }
+    
+    if(mymap.find(4)!=mymap.end())
+    {
+        cout<<"Found";
+    }
+    else
+    cout<<"Not found";
+
+}
+
+
+
+int main()
+{
+    hash1();
+    return 0;
+}
