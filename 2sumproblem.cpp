b47bf0d9f701d185666b5550082ec7399e9db71a
Pargat Singh <sandhu.pargat07@gmail.com>
2023-05-17 13:21:26 -0600

    2 sum

--- 2sumproblem.cpp
+++ 2sumproblem.cpp
@@ -1,9 +1,36 @@
 #include<iostream>
+#include<map>
+#include<vector>
 
 using namespace std;
 
+void twoSum(vector<int>& nums, int target)
+{
+    map<int, int> mymap;
+
+    for(int i =0;i<nums.size();i++)
+    {
+        int first = nums[i];
+        int second = target - first;
+
+        if(mymap.find(second)!=mymap.end())
+        {
+            cout<<mymap[second]<<i;
+        }
+        else 
+        mymap[first]=i;
+    }
+    
+
+}
+
 int main()
 {
-    int arr[5]={1,2,3,4,5,6};
+    vector <int> nums;
+    nums.push_back(2);
+    nums.push_back(7);
+    nums.push_back(11);
+    nums.push_back(15);
+    twoSum(nums, 9);
     return 0;
-    }
+}
